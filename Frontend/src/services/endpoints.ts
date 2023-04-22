342f252b7da0172c477311af2889063c88f0bf56
NoHop3 <u_sux@abv.bg>
2023-04-22 04:01:05 +0200

    Trying to fix eslint not being found when opening the root folder via-sem6-sep... Currently no idea how, so I open frontend & backend projects separately

--- Frontend/src/services/endpoints.ts
+++ Frontend/src/services/endpoints.ts
@@ -1,3 +1,4 @@
+// eslint-disable-next-line @typescript-eslint/no-non-null-assertion
 const baseUrl = process.env.REACT_APP_API_URL!;
 export const endpoints = {
   // Don't call this without first making it return a fixed amount!!! Current amount of movies over 350000
