346343667f626c381496af1b2164aef0939b1486
Restyled.io <commits@restyled.io>
2023-05-08 08:49:11 +0000

    Restyled by clang-format

--- Backend/Program.cs
+++ Backend/Program.cs
@@ -6,48 +6,42 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen(c =>
-{
-    c.SwaggerDoc("v1", new() {
-        Title = "Backend", Version = "v1"
-    });
+builder.Services.AddSwaggerGen(c => {
+  c.SwaggerDoc("v1", new() { Title = "Backend", Version = "v1" });
 });
 
 var provider = builder.Services.BuildServiceProvider();
-var configuration =builder.Configuration
-                   .AddJsonFile("appsettings.json")
-                   .AddJsonFile($"appsettings.{builder.Environment.EnvironmentName}.json")
-                   .AddEnvironmentVariables();//provider.GetRequiredService<IConfiguration>();
+var configuration =
+    builder.Configuration.AddJsonFile("appsettings.json")
+        .AddJsonFile($"appsettings.{builder.Environment.EnvironmentName}.json")
+        .AddEnvironmentVariables(); // provider.GetRequiredService<IConfiguration>();
 
-var connectionString = provider.GetRequiredService<IConfiguration>().GetConnectionString("DefaultConnection");
-builder.Services.AddDbContext<MyDbContext>(options =>
-{
-    options.UseSqlite(connectionString);
+var connectionString =
+    provider.GetRequiredService<IConfiguration>().GetConnectionString(
+        "DefaultConnection");
+builder.Services.AddDbContext<MyDbContext>(options => {
+  options.UseSqlite(connectionString);
 });
 
-builder.Services.AddCors(options =>
-{
-    var frontendUrl = provider.GetRequiredService<IConfiguration>().GetValue<string>("frontend_url")!;
-    options.AddDefaultPolicy(builder =>
-    {
-        builder.WithOrigins(frontendUrl)
-        .AllowAnyHeader()
-        .AllowAnyMethod();
-    });
+builder.Services.AddCors(options => {
+  var frontendUrl =
+      provider.GetRequiredService<IConfiguration>().GetValue<string>(
+          "frontend_url")!;
+  options.AddDefaultPolicy(builder => {
+    builder.WithOrigins(frontendUrl).AllowAnyHeader().AllowAnyMethod();
+  });
 });
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseDeveloperExceptionPage();
-    app.UseSwagger();
-    app.UseSwaggerUI(c =>
-    {
-        c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
-        c.RoutePrefix = "";
-    });
+if (app.Environment.IsDevelopment()) {
+  app.UseDeveloperExceptionPage();
+  app.UseSwagger();
+  app.UseSwaggerUI(c => {
+    c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+    c.RoutePrefix = "";
+  });
 }
 
 app.UseHttpsRedirection();
